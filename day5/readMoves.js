72a8cd587d76053c4be2be015d15611dd718c7a6
Matt Noakes <mattnoakes@me.com>
2022-12-15 23:18:13 +0000

    refactor

--- day5/readMoves.js
+++ day5/readMoves.js
@@ -1,28 +1,22 @@
 const readline = require("readline");
 const fs = require("fs");
 
+const movePattern = /move\s(\d+)\sfrom\s(\d+)\sto\s(\d+)/g;
+
+const parseMoveInput = (input) =>
+  input.startsWith("move")
+    ? Array.from(input.matchAll(movePattern)).flatMap((l) =>
+        l.slice(1).map(Number)
+      )
+    : [];
+
 module.exports = {
   readMoves: (path) =>
     new Promise((resolve) => {
-      const parseMoveInput = (input) =>
-        input.startsWith("move")
-          ? Array.from(
-              input.matchAll(/move\s(\d+)\sfrom\s(\d+)\sto\s(\d+)/g)
-            ).flatMap((l) => l.slice(1).map(Number))
-          : [];
-
       const moves = [];
-
       readline
         .createInterface({ input: fs.createReadStream(path) })
-        .on("line", (input) => {
-          const rawMove = parseMoveInput(input);
-          if (rawMove.length) {
-            moves.push(rawMove);
-          }
-        })
-        .on("close", () => {
-          resolve(moves);
-        });
+        .on("line", (input) => moves.push(parseMoveInput(input)))
+        .on("close", () => resolve(moves.filter((m) => m.length)));
     }),
 };
