ba50a60c49ddcf05406c396cf79d75eb83a53df0
Matt Noakes <mattnoakes@me.com>
2022-12-17 15:10:12 +0000

    day4 refactor

--- day4/part1and2.js
+++ day4/part1and2.js
@@ -1,6 +1,45 @@
-const attachment = require("../attachments");
-const CleanupAnalyser = require("./CleanupAnalyser");
+const { processInput } = require("../parser");
 
-const pathToFile = attachment("day4_cleanup.txt");
-const analyser = new CleanupAnalyser(pathToFile);
-analyser.analyse().then(console.log);
+/** Represents a contiguous range of cleaning assignments */
+class CleaningAssignment {
+  constructor(input) {
+    [this.min, this.max] = input.trim().split("-").map(Number);
+  }
+
+  get count() {
+    return this.max - this.min + 1;
+  }
+
+  /** Determines if a given assignment fully contains a second assignment */
+  fullyContains({ min, max, count }) {
+    return this.count >= count && this.min <= min && this.max >= max;
+  }
+
+  /** Determines if a given assignment overlaps at all with a second assignment */
+  overlapsWith({ min, max }) {
+    return Math.min(this.max, max) >= Math.max(this.min, min);
+  }
+}
+
+const fileName = "day4_cleanup.txt";
+
+(async function () {
+  const state = { contained: 0, overlapping: 0 };
+
+  await processInput(fileName, (input) => {
+    const [elf1Raw, elf2Raw] = input.trim().split(",");
+
+    const elf1 = new CleaningAssignment(elf1Raw);
+    const elf2 = new CleaningAssignment(elf2Raw);
+
+    if (elf1.fullyContains(elf2) || elf2.fullyContains(elf1)) {
+      state.contained += 1;
+    }
+    if (elf1.overlapsWith(elf2)) {
+      state.overlapping += 1;
+    }
+  });
+
+  console.log("Part 1", state.contained);
+  console.log("Part 2", state.overlapping);
+})();
