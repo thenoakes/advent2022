7db3f04b2d0218827638f077b076493f65c7cea5
Matt Noakes <mattnoakes@me.com>
2022-12-22 16:57:56 +0000

    day 8 done

--- day8/part1and2.js
+++ day8/part1and2.js
@@ -3,56 +3,65 @@ const { processInput } = require("../parser");
 const fileName = "day8_trees.txt";
 
 (async function () {
-  const state = {
-    rowCount: 0,
-    colCount: 0,
-    currentIndex: 0,
-    cols: [],
-    rows: [],
-  };
+  const state = { cols: [], rows: [], dimension: 0 };
+
   const initialise = (count) => {
-    state.rowCount = count;
-    state.colCount = count;
+    state.dimension = count;
   };
+
   const finalise = () => {
-    state.cols = Array.from({ length: state.colCount }).map((_, i) =>
+    state.cols = Array.from({ length: state.dimension }).map((_, i) =>
       state.rows.map((r) => r[i])
     );
   };
 
+  const findVisibleDistance = (array, height) => {
+    const index = array.findIndex((h) => h >= height);
+    return index === -1 ? array.length : index + 1;
+  };
+
   await processInput(fileName, (input) => {
     const line = input.trim();
-
-    if (state.currentIndex === 0) {
+    if (state.dimension === 0) {
       initialise(line.length);
     }
+    state.rows.push(line.split("").map((x) => parseInt(x)));
+  }).then(finalise);
 
-    const trees = line.split("").map((x) => parseInt(x));
-    state.rows.push(trees);
-    // console.log({ trees });
+  let scenicScore = 0;
+  let visibleTreeCount = 4 * state.dimension - 4;
+  for (let i = 1; i < state.dimension - 1; i++) {
+    for (let j = 1; j < state.dimension - 1; j++) {
+      const height = state.rows[i][j];
+      const score = { l: 0, r: 0, t: 0, b: 0 };
 
-    // for (let col = 0; col < state.colCount; col++) {
-    //   state.cols[col].push(trees[col]);
-    // }
+      const toLeft = state.rows[i].slice(0, j);
+      const toRight = state.rows[i].slice(j + 1);
+      const visibleHorizontal =
+        toLeft.every((h) => h < height) || toRight.every((h) => h < height);
 
-    // if (state.currentIndex === 0) {
-    //   state.cols[0].push(trees[0]);
-    //   console.log(state.cols);
-    // }
+      score.r = findVisibleDistance([...toRight], height);
+      score.l = findVisibleDistance([...toLeft].reverse(), height);
 
-    state.currentIndex += 1;
-  });
+      const above = state.cols[j].slice(0, i);
+      const below = state.cols[j].slice(i + 1);
+      const visibleVertical =
+        above.every((h) => h < height) || below.every((h) => h < height);
 
-  finalise();
+      score.b = findVisibleDistance([...below], height);
+      score.t = findVisibleDistance([...above].reverse(), height);
 
+      if (visibleHorizontal || visibleVertical) {
+        visibleTreeCount += 1;
+      }
 
-  // const column0 = state.rows.map((r) => r[0]);
-  // const column1 = state.rows.map((r) => r[1]);
-  // console.log(state.cols[98]);
+      scenicScore = Math.max(
+        scenicScore,
+        score.l * score.r * score.t * score.b
+      );
+    }
+  }
 
-  // for (let i = 0; i < state.colCount; i++) {
-  //   console.log({ value: state.rows[0][i] });
-  //   state.cols[i].push(state.rows[0][i]);
-  // }
-  // console.log(state.cols[0]);
+  console.log("Part 1", visibleTreeCount);
+  console.log("Part 2", scenicScore);
 })();
